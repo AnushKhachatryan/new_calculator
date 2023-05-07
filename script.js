5b39f0d6419af3c92fe6a213820cb7bcc3f5c65e
unknown <khachatryananush60@gmail.com>
2023-05-07 15:34:24 +0400

    First Commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const screen = document.querySelector("#screen");
+const buttons = document.querySelectorAll(".btn")
+
+let str = "";
+buttons.forEach((btn)=> {
+    btn.addEventListener("click", (b) => {
+        if(b.target.innerHTML === "C" ) {
+            str = "";
+            screen.value = str;
+        }else if (b.target.innerHTML === "=") {
+            str = eval(str)
+            screen.value = str
+        }else if (b.target.innerHTML === "Del"){
+            str = str.substring(0,str.length-1);
+            screen.value = str;
+        }else if(b.target.innerHTML === "^"){
+            str = str * str;
+            screen.value = str;
+        }else if(b.target.innerHTML === "√"){
+            str = Math.sqrt(str)
+            screen.value = str;
+        }
+        else{
+            str += b.target.innerHTML;
+            screen.value = str;
+        }
+    });
+})  
